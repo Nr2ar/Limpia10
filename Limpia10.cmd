2b53981a251324baef9acdb1a1c792d6a2460a32
Nr2ar <132923049+Nr2ar@users.noreply.github.com>
2023-05-12 09:16:20 -0300

    go

--- Limpia10.cmd
+++ Limpia10.cmd
@@ -12,7 +12,7 @@ echo.
 copy /Y "%~dp0Limpia10-update.cmd" "%~dp0Limpia10.cmd" >nul 2>&1
 
 
-set limpia10-URL=https://github.com/Nr2ar/Limpia10/raw/main/
+set limpia10-URL=https://raw.githubusercontent.com/Nr2ar/Limpia10/main/
 set curl="%~dp0curl.exe"
 set winlive=no
 set soywinlive=no
